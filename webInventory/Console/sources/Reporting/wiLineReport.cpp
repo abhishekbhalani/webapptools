c7032b4df9adcae946fac138aec821ac5d52a93c
stinger911 <stinger911@79c36d58-6562-11de-a3c1-4985b5740c72>
2009-08-31 14:13:24 +0000

    Reporting templates added. Buggi commit - XML->LoadFile crashes (if template file found)

---
+++ webInventory/Console/sources/Reporting/wiLineReport.cpp
@@ -0,0 +1,107 @@
+#include "wiLineReport.h"
+
+wiLineReport::wiLineReport(wxString msg /*= wxT("")*/, int lvl /*= 0*/)
+{
+    text = msg;
+    level = lvl;
+    LoadTemplate(wxT("linereport.xml"));
+}
+
+wiLineReport::~wiLineReport()
+{
+    //dtor
+}
+
+void wiLineReport::WriteReport(wxRichTextCtrl& rt)
+{
+    wxString attrib;
+    wxString style;
+    long    iData;
+    bool    isBold, isItalic, isUnder, isFontSize, isFontColor, isAlignment;
+
+    rt.SetInsertionPointEnd();
+    style = wxString::Format(wxT("level%d"), level);
+
+    isAlignment = GetStyleAttr(style, wxT("align"), attrib, wxT("4"));
+    if (!isAlignment) {
+        isAlignment = GetStyleAttr(wxT("default"), wxT("align"), attrib, wxT("4"));
+    }
+    if (isAlignment) {
+        attrib.ToLong(&iData);
+        rt.BeginAlignment((wxTextAttrAlignment)iData);
+    }
+
+    isBold = GetStyleAttr(style, wxT("font_bold"), attrib, wxT(""));
+    if (!isBold) {
+        isBold = GetStyleAttr(wxT("default"), wxT("font_bold"), attrib, wxT(""));
+    }
+    if (isBold) {
+        attrib.ToLong(&iData);
+        isBold = (iData != 0);
+        if (isBold) {
+            rt.BeginBold();
+        }
+    }
+
+    isItalic = GetStyleAttr(style, wxT("foint_italic"), attrib, wxT(""));
+    if (!isItalic) {
+        isItalic = GetStyleAttr(wxT("default"), wxT("foint_italic"), attrib, wxT(""));
+    }
+    if (isItalic) {
+        attrib.ToLong(&iData);
+        isItalic = (iData != 0);
+        if (isItalic) {
+            rt.BeginItalic();
+        }
+    }
+
+    isUnder = GetStyleAttr(style, wxT("foint_underline"), attrib, wxT(""));
+    if (!isUnder) {
+        isUnder = GetStyleAttr(wxT("default"), wxT("foint_underline"), attrib, wxT(""));
+    }
+    if (isUnder) {
+        attrib.ToLong(&iData);
+        isUnder = (iData != 0);
+        if (isUnder) {
+            rt.BeginUnderline();
+        }
+    }
+
+    isFontSize = GetStyleAttr(style, wxT("foint_size"), attrib, wxT("10"));
+    if (!isFontSize) {
+        isFontSize = GetStyleAttr(wxT("default"), wxT("foint_size"), attrib, wxT("10"));
+    }
+    if (isFontSize) {
+        attrib.ToLong(&iData);
+        rt.BeginFontSize(iData);
+    }
+
+    isFontColor = GetStyleAttr(style, wxT("foint_color"), attrib, wxT("black"));
+    if (!isFontColor) {
+        isFontColor = GetStyleAttr(wxT("default"), wxT("foint_color"), attrib, wxT("black"));
+    }
+    if (isFontColor) {
+        rt.BeginTextColour(wxColor(attrib));
+    }
+    rt.WriteText(text);
+    rt.WriteText(wxT("\n"));
+
+    if (isFontColor) {
+        rt.EndTextColour();
+    }
+    if (isFontSize) {
+        rt.EndFontSize();
+    }
+    if (isUnder) {
+        rt.EndUnderline();
+    }
+    if (isItalic) {
+        rt.EndItalic();
+    }
+    if (isBold) {
+        rt.EndBold();
+    }
+    if (isAlignment) {
+        rt.EndAlignment();
+    }
+}
