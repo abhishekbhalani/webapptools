298df8e920789d376478b45c5f06fa33d1a52b89
stinger911 <stinger911@79c36d58-6562-11de-a3c1-4985b5740c72>
2009-09-10 15:07:17 +0000

    iStorage plugin added for Redis DB

---
+++ webInventory/Plugins/Redis/dlMain.cpp
@@ -0,0 +1,27 @@
+/*
+    webEngine is the HTML processing library
+    Copyright (C) 2009 Andrew Abramov aabramov@ptsecurity.ru
+
+    This file is part of webEngine
+
+    webEngine is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    webEngine is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with webEngine.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "storageRedis.h"
+
+extern "C"
+void* WePluginFactory(void* krnl)
+{
+    Dispatch* kernel = (Dispatch*)krnl;
+    return (void*)(new FsStorage(kernel));
+}
