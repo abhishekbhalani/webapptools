d43e0f8bdf83a7e07b962a51685b0749248b2e3a
tursasp <tursasp@gmail.com>
2009-10-06 15:21:37 +0000

    scintilla lexer added

--- webInventory/Console/sources/wiService.cpp
+++ webInventory/Console/sources/wiService.cpp
@@ -1,10 +1,11 @@
 #include "wiService.h"
+#include "wiVulnDB.h"
 
 wiService::wiService( wxWindow* parent )
 :
 Service( parent )
 {
-    m_pVulners = new VulnDB( m_fnbService ); //, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL );
+    m_pVulners = new wiVulnDB( m_fnbService ); //, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL );
     m_fnbService->AddPage( m_pVulners, _("Vulnerabilities"), true );
     m_pReports = new RepDB( m_fnbService ); //, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL );
     m_fnbService->AddPage( m_pReports, _("Reports Templates"), false );
