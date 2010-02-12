64899be8b2fa1e45a61d23de248db23ad7c7cc25
tursasp <tursasp@gmail.com>
2010-02-12 21:16:03 +0000

    Redis info on dashboard

--- watools/console/dashboard.php
+++ watools/console/dashboard.php
@@ -9,7 +9,15 @@ if (!CheckACL('dashboard')) {
 	$smarty->assign('theme', $themeName);
 	$smarty->display('noaccess.html' . $themeLangExt);
 }
-
+$r = GetRedisConnection();
+if (!is_null($r)) {
+	$rInfo = $r->info();
+	$smarty->assign('redisVersion', $rInfo['redis_version']);
+	$smarty->assign('redisUptime', $rInfo['uptime_in_days']);
+	$smarty->assign('redisRole', $rInfo['role']);
+	$smarty->assign('redisMemory', $rInfo['used_memory']);
+	$smarty->assign('redisKeys', $rInfo['db' . $gRedisDB]);
+}
 $smarty->assign('UserName', $gUser[0]);
 $smarty->assign('theme', $themeName);
 $smarty->display('dashboard.html' . $themeLangExt);
