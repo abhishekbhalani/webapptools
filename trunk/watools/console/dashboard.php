50459dad39b9c559ab27c56a569310b9ca9ce2ab
tursasp@gmail.com <tursasp@gmail.com@79c36d58-6562-11de-a3c1-4985b5740c72>
2010-02-12 15:22:05 +0000

    dashboard preview

--- trunk/watools/console/dashboard.php
+++ trunk/watools/console/dashboard.php
@@ -1,3 +1,16 @@
 <?
-echo "<h1>Dashboard</h1>";
+require_once('./sessions.php');
+require_once('./themes.php');
+require_once('./usermgmt.php');
+
+// todo check ACL for access
+if (!CheckACL('dashboard')) {
+	$smarty->assign('UserName', $gUser[0]);
+	$smarty->assign('theme', $themeName);
+	$smarty->display('noaccess.html' . $themeLangExt);
+}
+
+$smarty->assign('UserName', $gUser[0]);
+$smarty->assign('theme', $themeName);
+$smarty->display('dashboard.html' . $themeLangExt);
 ?>
