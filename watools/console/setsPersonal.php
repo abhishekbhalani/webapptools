b082d8d636352fbbd03584609fbcc967b81a9a2f
tursasp <tursasp@gmail.com>
2010-03-03 12:32:35 +0000

    Theme management, personal settings

--- watools/console/setsPersonal.php
+++ watools/console/setsPersonal.php
@@ -9,6 +9,61 @@ if (!CheckACL('settings/personal')) {
     exit(0);
 }
 
+$r = GetRedisConnection();
+$thIDs = array();
+$thNms = array();
+$thDsc = array();
+$thLng = array();
+$thDef = -1;
+if (!is_null($r)) {
+    $thDef = $gUser[3];
+    $thDef = $r->get("ThemeName:$thDef");
+    $keys = $r->keys("ThemeName:*");
+    foreach ($keys as $k) {
+        $u = $r->get($k);
+        $k = substr($k, 10);
+        $thIDs[] = $u;
+        $thNms[] = $k;
+        $handle = @fopen($k . '/theme.txt', "r");
+        $data = gettext("No description provided");
+        $lng = array();
+        if ($handle) {
+            $data = fgets($handle);
+            $data = rtrim($data);
+            $locales = explode('|', $data);
+            foreach($locales as $l) {
+                $ln = explode(';', $l, 2);
+                $lng[] = $ln;
+            }
+            $data = "";
+            while (!feof($handle)) {
+                $data .= fgets($handle);
+            }
+            fclose($handle);
+        }
+        $thDsc[] = $data;
+        $thLng[] = $lng;
+    }
+}
+
+$lgIDs = array();
+$lgNms = array();
+$lgDef = $gUser[4];
+
+$lgIDs[] = 'en';
+$lgNms[] = 'English';
+$lgIDs[] = 'ru';
+$lgNms[] = 'Русский';
+
+$smarty->assign('themes', $thIDs);
+$smarty->assign('thNames', $thNms);
+$smarty->assign('thDesc', $thDsc);
+$smarty->assign('thLangs', $thLng);
+$smarty->assign('defTheme', $thDef);
+$smarty->assign('langs', $lgIDs);
+$smarty->assign('lnNames', $lgNms);
+$smarty->assign('defLang', $lgDef);
+
 $smarty->assign('UserName', $gUser[0]);
 $smarty->assign('userName', $gUser[1]);
 DisplayThemePage('settings/personal.html');
--- watools/console/setsSystem.php
+++ watools/console/setsSystem.php
@@ -13,18 +13,72 @@ $r = GetRedisConnection();
 $thIDs = array();
 $thNms = array();
 $thDsc = array();
+$thLng = array();
 $thDef = -1;
 if (!is_null($r)) {
     $thDef = $r->get("Global:DefaultTheme");
+    $thDef = $r->get("ThemeName:$thDef");
     $keys = $r->keys("ThemeName:*");
     foreach ($keys as $k) {
         $u = $r->get($k);
         $k = substr($k, 10);
         $thIDs[] = $u;
         $thNms[] = $k;
-        $thDsc[] = "Loren ipsum";
+        $handle = @fopen($k . '/theme.txt', "r");
+        $data = gettext("No description provided");
+        $lng = array();
+        if ($handle) {
+            $data = fgets($handle);
+            $data = rtrim($data);
+            $locales = explode('|', $data);
+            foreach($locales as $l) {
+                $ln = explode(';', $l, 2);
+                $lng[] = $ln;
+            }
+            $data = "";
+            while (!feof($handle)) {
+                $data .= fgets($handle);
+            }
+            fclose($handle);
+        }
+        $thDsc[] = $data;
+        $thLng[] = $lng;
     }
 }
+// search for not installed themes
+$dirs = glob('*', GLOB_ONLYDIR);
+if ($dirs) {
+    foreach ($dirs as $dir) {
+        $files = glob($dir . '/theme.txt');
+        if ($files) {
+            $tname = $dir;
+            if ($r->exists("ThemeName:$tname") == 0) {
+                $handle = @fopen($files[0], "r");
+                if ($handle) {
+                    $locstr = fgets($handle);
+                    $locstr = rtrim($locstr);
+                    $locales = explode('|', $locstr);
+                    $lng = array();
+                    foreach($locales as $l) {
+                        $ln = explode(';', $l, 2);
+                        $lng[] = $ln;
+                    }
+                    $data = "";
+                    while (!feof($handle)) {
+                        $data .= fgets($handle);
+                    }
+                    fclose($handle);
+                    $thIDs[] = 0;
+                    $thNms[] = $tname;
+                    $thDsc[] = $data;
+                    $thLng[] = $lng;
+                } // file handle opened
+            } // theme not installed
+        } // any files found
+    } // foraech dirs
+} // any dirs found
+
+
 $lgIDs = array();
 $lgNms = array();
 $lgDef = 'en';
@@ -37,6 +91,7 @@ $lgNms[] = 'Русский';
 $smarty->assign('themes', $thIDs);
 $smarty->assign('thNames', $thNms);
 $smarty->assign('thDesc', $thDsc);
+$smarty->assign('thLangs', $thLng);
 $smarty->assign('defTheme', $thDef);
 $smarty->assign('langs', $lgIDs);
 $smarty->assign('lnNames', $lgNms);
