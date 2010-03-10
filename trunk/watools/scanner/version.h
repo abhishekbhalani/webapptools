25250a775a7f147f7ef9979022f92100bc02dac0
stinger911 <stinger911@79c36d58-6562-11de-a3c1-4985b5740c72>
2010-03-10 15:05:15 +0000

    Add version numbering.

---
+++ trunk/watools/scanner/version.h
@@ -0,0 +1,29 @@
+/*-----------------------------------------------------------
+  VERSION CONTROL BUILD SYSTEM                               
+  This header file was created by VERBUILD v1.0.1            
+  -----------------------------------------------------------
+  help : verbuild -?                                         
+  info : http://www.yeamaec.com                              
+         yeamaec@hanafos.com ,krkim@yeamaec.com              
+-----------------------------------------------------------*/
+
+#ifndef VERSIONNO__H
+#define VERSIONNO__H
+
+#define VERSION_FULL           0.1.2.2
+
+#define VERSION_BASEYEAR       0
+#define VERSION_DATE           "2010-03-10"
+#define VERSION_TIME           "18:03:50"
+
+#define VERSION_MAJOR          0
+#define VERSION_MINOR          1
+#define VERSION_BUILDNO        2
+#define VERSION_EXTEND         2
+
+#define VERSION_FILE           0,1,2,2
+#define VERSION_PRODUCT        0,1,2,2
+#define VERSION_FILESTR        "0,1,2,2"
+#define VERSION_PRODUCTSTR     "0,1,2,2"
+
+#endif
