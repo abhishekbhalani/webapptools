53912a8c337afaf07db731271b5ff0f26a84e23d
stinger911 <stinger911@79c36d58-6562-11de-a3c1-4985b5740c72>
2010-03-26 15:53:01 +0000

    Some plugins-related operations.

--- trunk/watools/scanner/version.h
+++ trunk/watools/scanner/version.h
@@ -10,21 +10,21 @@
 #ifndef VERSIONNO__H
 #define VERSIONNO__H
 
-#define VERSION_FULL           0.1.3.168
+#define VERSION_FULL           0.1.9.187
 
 #define VERSION_BASEYEAR       0
-#define VERSION_DATE           "2010-03-25"
-#define VERSION_TIME           "17:58:39"
+#define VERSION_DATE           "2010-03-26"
+#define VERSION_TIME           "18:15:41"
 
 #define VERSION_MAJOR          0
 #define VERSION_MINOR          1
-#define VERSION_BUILDNO        3
-#define VERSION_EXTEND         168
+#define VERSION_BUILDNO        9
+#define VERSION_EXTEND         187
 
-#define VERSION_FILE           0,1,3,168
-#define VERSION_PRODUCT        0,1,3,168
-#define VERSION_FILESTR        "0,1,3,168"
-#define VERSION_PRODUCTSTR     "0,1,3,168"
+#define VERSION_FILE           0,1,9,187
+#define VERSION_PRODUCT        0,1,9,187
+#define VERSION_FILESTR        "0,1,9,187"
+#define VERSION_PRODUCTSTR     "0,1,9,187"
 
 #define VERSION_SVN            "SVN $Revision$"
 #endif
