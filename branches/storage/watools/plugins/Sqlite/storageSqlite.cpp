97f78fded31366c500ae6b7c29bd38386b7d2685
stinger911 <stinger911@79c36d58-6562-11de-a3c1-4985b5740c72>
2010-07-01 19:57:58 +0000

    sqlite_storage plugin reanimated and added to demo project: crawler

--- branches/storage/watools/plugins/Sqlite/storageSqlite.cpp
+++ branches/storage/watools/plugins/Sqlite/storageSqlite.cpp
@@ -5,8 +5,8 @@
     This file is part of webEngine
 
     webEngine is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
     webEngine is distributed in the hope that it will be useful,
@@ -14,7 +14,7 @@
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
+    You should have received a copy of the GNU Lesser General Public License
     along with webEngine.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include <boost/lexical_cast.hpp>
@@ -22,84 +22,91 @@
 #include "storageSqlite.h"
 #include "version.h"
 #include "sqliteStorage.xpm"
-//#include "demoPlugin.xrc"
+#include "sqlite3.h"
 
-SqliteStorage::SqliteStorage( Dispatch* krnl, void* handle /*= NULL*/ ) :
-    iStorage(krnl, handle)
+struct sqlite_handle
 {
-    pluginInfo.IfaceName = "SqliteStorage";
-    pluginInfo.IfaceList.push_back("SqliteStorage");
-    pluginInfo.PluginDesc = "Sqlite storage";
-    pluginInfo.PluginId = "9FBCC44C52A1";
-    pluginInfo.PluginIcon = WeXpmToStringList(sqliteStorage_xpm, sizeof(sqliteStorage_xpm) / sizeof(char*) );
+};
+
+sqlite_storage::sqlite_storage( engine_dispatcher* krnl, void* handle /*= NULL*/ ) :
+    i_storage(krnl, handle)
+{
+    pluginInfo.interface_name = "sqlite_storage";
+    pluginInfo.interface_list.push_back("sqlite_storage");
+    pluginInfo.plugin_desc = "Sqlite storage";
+    pluginInfo.plugin_id = "9FBCC44C52A1";
+    pluginInfo.plugin_icon = WeXpmToStringList(sqliteStorage_xpm, sizeof(sqliteStorage_xpm) / sizeof(char*) );
     LOG4CXX_TRACE(logger, "SqliteStorage plugin created");
 }
 
-SqliteStorage::~SqliteStorage(void)
+sqlite_storage::~sqlite_storage(void)
 {
     LOG4CXX_TRACE(logger, "SqliteStorage plugin destroyed");
 }
 
-void* SqliteStorage::GetInterface( const string& ifName )
+i_plugin* sqlite_storage::get_interface( const string& ifName )
 {
-    LOG4CXX_TRACE(logger, "SqliteStorage::GetInterface " << ifName);
-    if (iequals(ifName, "SqliteStorage"))
+	if (boost::iequals(ifName, "i_storage"))
     {
         usageCount++;
-        return (void*)(this);
+        return ((i_storage*)this);
     }
-    return iStorage::GetInterface(ifName);
+    if (boost::iequals(ifName, "sqlite_storage"))
+    {
+        usageCount++;
+        return ((sqlite_storage*)this);
+    }
+    return i_storage::get_interface(ifName);
 }
 
-const string SqliteStorage::GetSetupUI( void )
+int sqlite_storage::get(db_query& query, db_recordset& results)
 {
-    /// @todo: change XRC to set the fields values
-    return "";
-}
-
-void SqliteStorage::ApplySettings( const string& xmlData )
-{
-
-}
-
-int SqliteStorage::Get(Record& filter, Record& respFilter, RecordSet& results)
-{
-    LOG4CXX_TRACE(logger, "SqliteStorage::Get objType=" << filter.objectID);
+    LOG4CXX_TRACE(logger, "SqliteStorage::get");
     /// @todo Implement this
     LOG4CXX_FATAL(iLogger::GetLogger(), "SqliteStorage::Get not implemented!");
     return 0;
 }
 
-int SqliteStorage::Set(Record& filter, Record& data)
+int sqlite_storage::set(db_query& query, db_recordset& data)
 {
-    LOG4CXX_TRACE(iLogger::GetLogger(), "SqliteStorage::Set(Record, Record); objType=" << filter.objectID);
+    LOG4CXX_TRACE(iLogger::GetLogger(), "SqliteStorage::Set(db_query, db_recordset)");
     /// @todo Implement this
     LOG4CXX_FATAL(iLogger::GetLogger(), "SqliteStorage::Set not implemented!");
     return 0;
 }
 
-int SqliteStorage::Set(RecordSet& data)
+int sqlite_storage::set(db_recordset& data)
 {
-    int retval;
-    size_t i;
-    Record filt;
-
-    LOG4CXX_TRACE(iLogger::GetLogger(), "SqliteStorage::Set(RecordSet)");
-    filt.Clear();
-    retval = 0;
-    for (i = 0; i < data.size(); i++)
-    {
-        filt.objectID = data[i].objectID;
-        retval += Set(filt, data[i]);
-    }
-
-    return retval;
+    LOG4CXX_TRACE(iLogger::GetLogger(), "SqliteStorage::Set(db_recordset)");
+    /// @todo Implement this
+    LOG4CXX_FATAL(iLogger::GetLogger(), "SqliteStorage::Set not implemented!");
+    return 0;
 }
 
-int SqliteStorage::Delete(Record& filter)
+int sqlite_storage::del(db_filter& filter)
 {
-    LOG4CXX_TRACE(iLogger::GetLogger(), "SqliteStorage::Delete; objType=" << filter.objectID);
+    LOG4CXX_TRACE(iLogger::GetLogger(), "SqliteStorage::Delete;");
     /// @todo Implement this
     LOG4CXX_FATAL(iLogger::GetLogger(), "SqliteStorage::Delete not implemented!");
     return 0;
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/// @fn bool sqlite_storage::init_storage( const string& params )
+///
+/// @brief  Initializes the storage_db. 
+///
+/// @param  params - Pathname for the storage_db file.
+/// @retval	true if it succeeds, false if it fails. 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+bool sqlite_storage::init_storage( const string& params )
+{
+	bool result = false;
+    boost::unique_lock<boost::mutex> locker(data_access);
+
+	return result;
+}
+
+//void sqlite_storage::flush( const string& params /*= ""*/)
+//{
+//}
