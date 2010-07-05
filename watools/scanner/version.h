bdb4073273253a70d5b898e6b9cb8256eef979c7
stinger911 <stinger911@79c36d58-6562-11de-a3c1-4985b5740c72>
2010-07-05 06:51:02 +0000

    Save for merge

--- watools/scanner/version.h
+++ watools/scanner/version.h
@@ -10,21 +10,21 @@
 #ifndef VERSIONNO__H
 #define VERSIONNO__H
 
-#define VERSION_FULL           0.1.9.187
+#define VERSION_FULL           0.1.10.187
 
 #define VERSION_BASEYEAR       0
-#define VERSION_DATE           "2010-03-26"
-#define VERSION_TIME           "18:15:41"
+#define VERSION_DATE           "2010-05-19"
+#define VERSION_TIME           "14:07:46"
 
 #define VERSION_MAJOR          0
 #define VERSION_MINOR          1
-#define VERSION_BUILDNO        9
+#define VERSION_BUILDNO        10
 #define VERSION_EXTEND         187
 
-#define VERSION_FILE           0,1,9,187
-#define VERSION_PRODUCT        0,1,9,187
-#define VERSION_FILESTR        "0,1,9,187"
-#define VERSION_PRODUCTSTR     "0,1,9,187"
+#define VERSION_FILE           0,1,10,187
+#define VERSION_PRODUCT        0,1,10,187
+#define VERSION_FILESTR        "0,1,10,187"
+#define VERSION_PRODUCTSTR     "0,1,10,187"
 
 #define VERSION_SVN            "SVN $Revision$"
 #endif
